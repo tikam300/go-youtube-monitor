ced545af1482c48f42f4f25f91a14b602b63206c
tikam300 <tikamsingh2402@gmail.com>
2025-08-11 23:42:19 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/tikam300/go-youtube-monitor/websocket"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
+
+func Stats(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+	go websocket.Writer(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/stats", Stats)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	fmt.Println("Youtube Subscriber monitor")
+
+	setupRoutes()
+}
